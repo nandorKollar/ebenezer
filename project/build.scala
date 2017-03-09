ec5268eaeece0b75e6d2554a182b8781fa047746
Andreas Nygard <andreas.nygard@gmail.com>
2017-03-09 16:01:41 +1100

    Automatic version update (#164)
    * Bump version to 0.23.10
    
    * thermometer: 1.5.5-20170221075632-abcad7c -> 1.5.6-20170303043502-111464d
    
    * beeswax: 0.1.10-20170221084442-addbf4a -> 0.1.11-20170309043604-a170988

--- project/build.scala
+++ project/build.scala
@@ -27,10 +27,10 @@ import au.com.cba.omnia.uniform.assembly.UniformAssemblyPlugin._
 import au.com.cba.omnia.humbug.HumbugSBT._
 
 object build extends Build {
-  val thermometerVersion   = "1.5.5-20170221075632-abcad7c"
+  val thermometerVersion   = "1.5.6-20170303043502-111464d"
   val omnitoolVersion      = "1.14.6-20170221081926-378a521"
   val humbugVersion        = "0.7.7-20170221075514-a8665a9"
-  val beeswaxVersion       = "0.1.10-20170221084442-addbf4a"
+  val beeswaxVersion       = "0.1.11-20170309043604-a170988"
 
   lazy val standardSettings =
     Defaults.coreDefaultSettings ++
