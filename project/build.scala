82741c1d7f93eec0c7d64236e81fbacee43ba894
Sam Roberts <sam.roberts@cba.com.au>
2018-02-26 00:55:01 +1100

    thermometer: 1.6.0-20180124000127-aec09bd-cdh-513 -> 1.6.1-20180225133458-8761e51-cdh-513

--- project/build.scala
+++ project/build.scala
@@ -27,7 +27,7 @@ import au.com.cba.omnia.uniform.assembly.UniformAssemblyPlugin._
 import au.com.cba.omnia.humbug.HumbugSBT._
 
 object build extends Build {
-  val thermometerVersion   = "1.6.0-20180124000127-aec09bd-cdh-513"
+  val thermometerVersion   = "1.6.1-20180225133458-8761e51-cdh-513"
   val omnitoolVersion      = "1.15.1-20180225131547-b10cb5e-cdh-513"
   val humbugVersion        = "0.8.0-20180124005627-cf7d6c4-cdh-513"
   val beeswaxVersion       = "0.2.1-20180124053822-85eba7a-cdh-513"
