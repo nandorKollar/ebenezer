8a0537f5ee01e614088fe2e0a0b3f04ded494a2c
Autowrangler <autowrangler@cba.com.au>
2018-03-06 14:56:33 +0000

    humbug: 0.8.1-20180225135322-2b1c6b8-cdh-513 -> 0.8.2-20180306144326-8b1b4f3-cdh-513

--- project/build.scala
+++ project/build.scala
@@ -29,7 +29,7 @@ import au.com.cba.omnia.humbug.HumbugSBT._
 object build extends Build {
   val thermometerVersion   = "1.6.1-20180225133458-8761e51-cdh-513"
   val omnitoolVersion      = "1.15.1-20180225131547-b10cb5e-cdh-513"
-  val humbugVersion        = "0.8.1-20180225135322-2b1c6b8-cdh-513"
+  val humbugVersion        = "0.8.2-20180306144326-8b1b4f3-cdh-513"
   val beeswaxVersion       = "0.2.2-20180225143455-2935b1f-cdh-513"
 
   lazy val standardSettings =
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -28,4 +28,4 @@ addSbtPlugin("au.com.cba.omnia" % "uniform-thrift"     % uniformVersion)
 
 addSbtPlugin("au.com.cba.omnia" % "uniform-assembly"   % uniformVersion)
 
-addSbtPlugin("au.com.cba.omnia" % "humbug-plugin"      % "0.8.1-20180225135322-2b1c6b8-cdh-513")
+addSbtPlugin("au.com.cba.omnia" % "humbug-plugin"      % "0.8.2-20180306144326-8b1b4f3-cdh-513")
